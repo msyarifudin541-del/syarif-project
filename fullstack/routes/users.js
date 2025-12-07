7b94c377ea7611c41c12062bbb2fd441dd25547f
Muhammad Syarifudin <m.syarifudin541@gmail.com>
2025-12-07 12:02:44 +0700

    uda bisa merubah data

--- fullstack/routes/users.js
+++ fullstack/routes/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const UsersModel = require('../models/users');
+const UsersModel = require('../models/users'); 
 
 // routing endpoit users utama
 router.get('/', async (req, res) => {
     const users = await UsersModel.findAll();
     res.status(200).json({
-        data: 'backend 1',
+        data: users,
         metadata: "test user endpoint"
     })
 });
@@ -23,5 +23,29 @@ router.post('/', async (req, res) => {
         metadata: "test post user endpoint"
     })
 }) ;
+
+router.put('/', async (req, res) => {
+    //nip, nama, password -->>>>>>>>>>>> Backend nagkep
+    const { nip, nama, password, passwordBaru } = req.body;
+
+    const userData = await UsersModel.findOne({ where: { nip: nip } });
+
+    //password yang muncul di db ==== password dari inputan
+    if (userData.password === password) {
+
+        const users = await UsersModel.update({
+            nama, password: passwordBaru
+        },   { where: { password: userData.password } });  
+    
+        res.json({
+            users
+        });
+
+    }   else {
+        res.json({
+            error: " Data tidak ditemukan / password salah"
+        })
+    }  
+})
 
 module.exports = router;
